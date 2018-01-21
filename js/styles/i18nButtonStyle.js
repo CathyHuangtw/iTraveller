67eaa82dbf6f6e7dfd163c08f6680b832f9a2664
Cathy Huang <cathyhaung.tw@gmail.com>
2018-01-21 12:37:06 -0800

    update style file

--- js/styles/i18nButtonStyle.js
+++ js/styles/i18nButtonStyle.js
@@ -1,15 +1,6 @@
-import {purple300} from 'material-ui/styles/colors';
-import ribbonimg from '../../img/purple-ribbon.png';
-
-const imgUrl = `url(${ribbonimg})`;
 
 export default {
   main: {
-    background: {
-      image: imgUrl,
-      repeat: 'no-repeat',
-      size: [90, 150],
-    },
     height: '155px',
     position: 'relative',
     left: '18%'
